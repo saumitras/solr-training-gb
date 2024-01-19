c37de2604f23d473cbeaaa2dfe4b7ad19fb4b91b
saumitras <saumitra.srivastav7@gmail.com>
2024-01-19 12:48:51 +0000

    working

--- 8.11.2/solr-llm/src/main/scala/index/IndexAuth.scala
+++ 8.11.2/solr-llm/src/main/scala/index/IndexAuth.scala
@@ -27,6 +27,15 @@ object IndexAuth extends App {
   println("Set a default collection")
   client.setDefaultCollection(collectionName)
 
+  val corpus = List(
+    "Illegal request, responding with status '400 Bad Request': Unsupported HTTP method: The HTTP method started with 0x16 rather than any known HTTP method from 10.15.40.249:2480. Perhaps this was an HTTPS request sent to an HTTP endpoint?",
+    "MLManager was not able to truncate existing data from AE's operational tables stored in AE_ML_DB in H2. Until old entries are deleted from AE, new model or ae config changes can't be deployed.",
+    "Exact reason for the error will be visible in the exception message. Most common reason is unreachable H2 host, either because of misconfiguration or problem with exposed service port. Other possible reasons for this are unreachable H2 host, incorrect H2 config in deployment.json, unavailable H2, change in DDL, timeouts in case of huge data, or referential integrity bugs. Ref integrity errors needs to be assigned to devs. Remaining errors needs to be fixed by devops.",
+    "MLManager failed to parse deployment json. Deployment.json located in mlops repo at https://github.com/glassbeam/mlops/blob/master/config/dashboard.json is central file to configure all infra and decide which ML services will be deployed in a given environment. The format of this file is fixed and if you add any new unsupported field or if there is a syntax error, you will get this error. All downstream applications will fail to start until you resolve this issue. The downstream application which are already running will continue to run, but will fail to sync with latest changes, so treat this issue as fatal.",
+    "First ensure that deployment.json is in valid JSON format with no syntax issues like missing commas, unclosed brackets. Then ensure that mlmanager support all the fields which are configure in that file. It might be possible that mlmanager is running on old version and doesn't understand latest format of deployment.json. If issue still persist, share mlmanager URL and commit ID of deployment.json or mlops repo which you used while starting mlmanager, with dev.",
+    "Prometheus HTTP service couldn't be started for MLManager service. Due to this metrics will not be picked up by central prometheus scraper and alerts will not be raised by alert manager.",
+    "Most likely reason for this error is that the port designated for prometheus server is being used by another service. Another less likely reason is host box resource starvation."
+  )
   println("Adding document")
   for (i <- 1 to numDocs) {
     val doc = new SolrInputDocument()
--- 8.11.2/solr-llm/src/main/scala/index/dv/VectorUtils.scala
+++ 8.11.2/solr-llm/src/main/scala/index/dv/VectorUtils.scala
@@ -10,10 +10,12 @@ import org.apache.spark.sql.functions._
 
 object VectorUtils extends App {
 
-  val resp = getVectorRepresentationOfTextUsingBERT("This is a test sentence").mkString("Array(", ", ", ")")
-  println(resp)
+  val resp = getVectorRepresentationOfTextUsingBERT("sentence")
+//  println(resp(0).mkString(","))
+//  println(resp.length)
 
-  def getVectorRepresentationOfTextUsingBERT(text: String):Array[Seq[Double]] = {
+
+  def getVectorRepresentationOfTextUsingBERT(text: String) = {
     val spark = SparkSession.builder()
       .appName("BERT Example")
       .master("local[*]")
@@ -49,13 +51,30 @@ object VectorUtils extends App {
     val model = pipeline.fit(data)
     val result = model.transform(data)
 
-    // Extract the embeddings from the WordEmbeddings
+
+// This will return an `Array[Seq[Double]]`, where each `Seq[Double]` is the embedding vector for a token in the text. The size of the response vector is the total number of tokens in the text multiplied by the dimension of the embeddings.
+//    val vectors = result.select("finished_bert")
+//      .collect()
+//      .map(row => row.getAs[Seq[DenseVector]](0).map(_.toArray).flatten)
+//
+// If you want to get a single vector of size 768 for the entire text, you need to aggregate the embeddings of all tokens. One common way to do this is by averaging the embeddings.
+
+
+// Extract the embeddings from the WordEmbeddings
+
     val vectors = result.select("finished_bert")
       .collect()
-      .map(row => row.getAs[Seq[DenseVector]](0).map(_.toArray).flatten)
+      .flatMap(row => row.getAs[Seq[DenseVector]](0).map(_.toArray))
+
+    // Average the embeddings
+    val avgVector = vectors.transpose.map(_.sum / vectors.length)
+
+    avgVector
 
 
-    vectors
+//    result.select("finished_bert")
+//        .collect()
+//        .map(row => row.getAs[Seq[DenseVector]](0).map(_.toArray).flatten)
   }
 
 }
